cbf7302aadd21f573fc4b7268bb254093b588826
Joelle Shane <joelle.shane@gmail.com>
2016-10-10 13:25:22 -0700

    update

--- lab7/test-that/functions/range-value.R
+++ lab7/test-that/functions/range-value.R
@@ -1,7 +1,12 @@
-range_value <- function(x, y) {
-b <- max(x) - min(x) 
-if (y == TRUE) {
-na.rm(b)
+range_value <- function(x, na.rm = FALSE) {
+if (na.rm) {
+	for (i in (1:length(x))) {
+		if (is.na(i) == FALSE) { b[i] <- b[i]
+			}
+	} 
+d <- max(b) - min(b) } 
+else {
+	d <- max(x) - min(x)
 }
-return (b)
+return (d)
 }
--- lab7/test-that/tests/tests1.R
+++ lab7/test-that/tests/tests1.R
@@ -3,5 +3,5 @@ context("testing y")
 test_that("testing range value with y", {
 y <- c(1, 2, 3, 4, NA)
 
-expect_length(range_value(y), 1)
-expect_equal(range_value(y), "NA_real_") })
+expect_length(range_value(y, na.rm = TRUE), 1)
+expect_equal(range_value(y, na.rm = TRUE), "NA_real_") })
