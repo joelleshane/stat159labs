061e989960ec7f899f69b9c8d5c17a0b81b28978
Joelle Shane <joelle.shane@gmail.com>
2016-10-24 13:21:58 -0700

    correcting lab8

---
+++ lab8/app.R
@@ -0,0 +1,17 @@
+```{r, echo = FALSE}
+ad <- read.csv("Advertising.csv")
+library(ggplot2)
+library(shiny)
+
+ui <- fluidPage(selectInput(inputId = "Method", label = "Advertising Method", choices = c("TV", "Newspaper", "Radio")), 
+                  plotOutput('scatterplot'))
+
+server <- function(input, output){
+  output$scatterplot <- renderPlot({plot(ad[,input$method], ad$Sales,
+                                         xlab = input$method, ylab = "Sales", main = "Effect of Advertising on Sales")})
+}
+
+shinyApp(ui = ui, server = server)
+
+```
+
